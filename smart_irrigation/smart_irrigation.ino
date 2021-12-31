efa65fdccd669d6cb0773452771ad31db9009493
Pavan S <pavans.shetty.2452@gmail.com>
2021-12-31 18:18:01 +0530

    update m2m

--- smart_irrigation/smart_irrigation.ino
+++ smart_irrigation/smart_irrigation.ino
@@ -1,35 +1,46 @@
 #include "DHT.h"
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
-
+#define DHTPIN 2// Digital pin connected to the DHT sensor
+#define soil_s A0
 #define DHTTYPE DHT11   // DHT 11
-int water,temp, temp1; //random variable 
+float soil_moist; //random variable 
+
 DHT dht(DHTPIN, DHTTYPE);
+
+int motor_on();
+
 void setup() {
-  pinMode(3,OUTPUT); //output pin for relay board, this will sent signal to the relay
-  pinMode(6,INPUT); //input pin coming from soil sensor
+  pinMode(3,OUTPUT); 
+  pinMode(1,INPUT);
   Serial.begin(9600);
+  
   dht.begin();
 }
 
 void loop() { 
-  water = digitalRead(6);
-   temp=analogRead(A0);// reading the coming signal from the soil sensor
-  if(water == HIGH) // if water level is full then cut the relay 
+  soil_moist = analogRead(soil_s);
+  soil_moist = map(soil_moist,550,10,0,100);
+  Serial.print(soil_moist);
+  Serial.println("%");
+  if(soil_moist <= 0) 
   {
-  digitalWrite(3,LOW); // low is to cut the relay
-  printf("%s",water);
-  
+    motor_on();
   }
   else
   {
-  digitalWrite(3,HIGH); //high to continue proving signal and water supply
+    digitalWrite(3,HIGH); 
   }
-//  Serial.println(water);
-//  Serial.println(digitalRead(8));
+    Serial.print(dht.readHumidity());
+    Serial.println("hum ");
+    Serial.print(dht.readTemperature());
+    Serial.println("C ");
+    Serial.println("******");
 
- temp1=temp*0.48828125;
-    Serial.println(dht.readHumidity());
-    Serial.println(dht.readTemperature());
-  delay(1000); 
+  delay(10000); 
+}
+
+int motor_on()
+{
+   digitalWrite(3,LOW); 
+   Serial.println("motor on @"); 
 }
